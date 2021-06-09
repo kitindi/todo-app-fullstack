fd408aba6b07c4ad6e187f04f70eaa70db2ed37f
kitindi <abdullkitindi@gmail.com>
2021-06-09 17:24:35 +0300

    connected app to mongoDB using mongoose

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
-const date = require(__dirname + '/date.js')
 const app = express();
 
-const items= [];
+// connecting to the database
+// import mongodb library mongoose & 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 //set up ejs
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+
+
+// defining scheme
+
+const itemsSchema =  new mongoose.Schema({
+   name: String
+})
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({name: "Welcome to your todo list"})
+const item2 = new Item({name: "Hit + button to add a new item"})
+const item3 = new Item({name: "Hit <---- button to delete an item"})
+
+
+const defaultItems = [item1, item2, item3]
+
+
+Item.insertMany(defaultItems, function(err){
+  if(err){
+    console.log(err)
+  }else{
+    console.log("sucsessfully added")
+  }
+})
 
 // accesing the static files
 
@@ -15,13 +42,11 @@ app.use(express.static('public'));
 app.use('css', express.static(__dirname + '/public/css'))
 
 // express body-parser
-
 app.use(express.urlencoded())
 
 app.get('/', (req, res)=>{
-     const day = date()
 
-     res.render('index', {day :day, newTodo: items});
+     res.render('index', {day :"Today", newTodo: items});
 })
 
 app.post('/', (req, res)=>{
