46f160205002bd8edb2d712e61ca6e279207c34f
kitindi <abdullkitindi@gmail.com>
2021-06-11 10:46:52 +0300

    connedted app with mongoDB Atlas cloud database

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.urlencoded())
 // connecting to the database
 // import mongodb library mongoose & 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin-abdulaziz:abdulazizkitindi888@cluster0.ucnsv.mongodb.net/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 //set up ejs
 app.set('views', './views');
@@ -27,15 +27,6 @@ const item3 = new Item({name: "Hit <---- button to delete an item"})
 const defaultItems = [item1, item2, item3]
 
 
-// Item.insertMany(defaultItems, function(err){
-//   if(err){
-//     console.log(err)
-//   }else{
-//     console.log("sucsessfully added")
-//   }
-// })
-
-// accesing the static files
 
 app.use(express.static('public'));
 app.use('css', express.static(__dirname + '/public/css'))
@@ -63,6 +54,35 @@ app.get('/', (req, res)=>{
    })
 })
 
+//Express dynamic route
+
+// app.get("/:customListName", (req, res)=>{
+//   const customeListName = req.params.customListName;
+
+//    List.findOne({name:customeListName}, (err, findList)=>{
+//     if(!err){
+//       if(!findList){
+//         console.log("doesn't exists")
+//         //create a list
+//         const list = new List({
+//         name:customeListName,
+//         items: defaultItems
+//         })
+//        list.save();
+//       }else{
+//         //show existing list
+//         console.log("exists")
+
+//         res.render('list',{day :findList.name, newTodo:findList.items} )
+//       }
+//     }
+//    })  
+// })
+
+
+
+
+
 app.post('/', (req, res)=>{
   let itemName = req.body.todo;
   //creating new item to save in database
@@ -72,6 +92,8 @@ app.post('/', (req, res)=>{
   }
    res.redirect('/'); 
 })
+
+
 
 app.post('/delete', (req, res)=>{
   const checkedItemId =req.body.checkbox;
