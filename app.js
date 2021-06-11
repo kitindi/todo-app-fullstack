aa23a7bcb51d5f788c4c8ca39b5b9578ecc9cf8b
kitindi <abdullkitindi@gmail.com>
2021-06-11 12:14:51 +0300

    Added Procfile for heroku hosting

--- app.js
+++ app.js
@@ -108,6 +108,6 @@ app.post('/delete', (req, res)=>{
   })
 })
 
-app.listen(3000, (req, res)=>{
+app.listen(process.env.PORT || 3000, (req, res)=>{
     console.log('Server is running on port 3000');
 })
